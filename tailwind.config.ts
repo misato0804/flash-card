7a1e9aabbecced28d86734708dd447dc95e7655e
Misato Tanno <breitmeer.0804@gmail.com>
2024-04-11 13:40:06 -0600

    Fixed Tailwind fontWeight type

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,16 +17,16 @@ const config: Config = {
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
             },
             fontWeight: {
-                'hairline': 100,   // Custom extra light weight
-                'thin': 200,
-                'light': 300,
-                'normal': 400,
-                'medium': 500,
-                'semibold': 600,
-                'bold': 700,
-                'extrabold': 800,
-                'black': 900,
-                'extra': 950      // Custom extra bold weight
+                'hairline': '100',
+                'thin': '200',
+                'light': '300',
+                'normal': '400',
+                'medium': '500',
+                'semibold': '600',
+                'bold': '700',
+                'extrabold': '800',
+                'black': '900',
+                'extra': '950'
             }
         },
     },
